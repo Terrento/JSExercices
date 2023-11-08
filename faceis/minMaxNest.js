0360f4c8be6f10a6c3ac4033b5abd79539a5519f
Fabrício <fabricioferreira900@gmail.com>
2023-11-08 20:44:22 -0300

    Just some corrections

--- faceis/minMaxNest.js
+++ faceis/minMaxNest.js
@@ -1,3 +1,6 @@
+/*This program checks if the first array can be nested in the second
+Original problem here: https://edabit.com/challenge/Gpy2qSFnfhGJnWMMj
+*/
 const canNest = (list1, list2) =>{
     if (Math.min(...list1) > Math.min(...list2) &&
     Math.max(...list1) < Math.max(...list2)){
